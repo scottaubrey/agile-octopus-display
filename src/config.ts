96e0d09549cc64e65e4c2a0b98e4e7a23dfcec82
Scott Aubrey <scott@aubrey.org.uk>
2023-09-24 09:31:11 +0100

    add a config from env to use for client params

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  octopus: {
+    apiKey: process.env.OCTOPUS_APIKEY,
+    productCode: process.env.OCTOPUS_PRODUCT_KEY,
+    regionCode: process.env.OCTOPUS_REGION_CODE,
+    mpan: process.env.OCTOPUS_MPAN,
+    serialNumber: process.env.OCTOPUS_SERIAL_NUMBER,
+  },
+};
